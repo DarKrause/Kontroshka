b77635457e07299101177116ffe2b3d7d5d0c05a
denus <denus@DESKTOP-IUOKO40>
2022-03-10 15:16:22 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary1
+{
+    public class Study
+    {
+        public class Mark
+        {
+            public Mark(DateTime _date, string _Estimation)
+            {
+                this.date = _date;
+                this.Estimation = _Estimation;
+            }
+            DateTime date { get; set; } // дата оценки
+            string Estimation { get; set; } // оценнка или отметка об отсутствии
+        }
+
+        public class Student
+        {
+            public Student(string _FIO, int _group, int _year)
+            {
+                this.FIO = _FIO;
+                this.group = _group;
+                this.year = _year;
+            }
+            string FIO { get; set; } // ФИО студента
+            int group { get; set; } // группа
+            int year { get; set; } // год поступления
+
+            List<Mark> marks { get; set; } //оценки студента
+
+            public double MinAVG(string[] marks) // вычисление средней оценки
+            {
+                double avg = 0;
+                int count = 0;
+                for (int i = 0; i < marks.Length; i++)
+                {
+                    int num;
+                    bool isNum = int.TryParse(marks[i], out num); //проверяем является ли текущая строка числом
+                    if (isNum)
+                    {
+                        avg += Convert.ToInt32(marks[i]);
+                        count++;
+                    }
+                }
+                avg = avg / count;
+                avg = avg - (avg % 1);
+                return avg;
+            }
+
+            public int GetCountTruancy(List<Mark> marks) // вычисление количества прогулов
+            {
+                return 0;
+            }
+        }
+    }
+
+
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double expected = 4;
+            ClassLibrary1.Study.Student student = new ClassLibrary1.Study.Student("Мещанов Вадим Сергеевич", 1281, 2018);
+            string[] marks = new string[2] { "4", "5" };
+            double actual = student.MinAVG(marks);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
